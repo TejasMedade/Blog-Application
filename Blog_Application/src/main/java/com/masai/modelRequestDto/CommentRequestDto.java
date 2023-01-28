4bbb2a2e5930de63d674723ed5662811a8aab015
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:26:53 +0530

    Update CommentRequestDto.java

--- Blog_Application/src/main/java/com/masai/modelRequestDto/CommentRequestDto.java
+++ Blog_Application/src/main/java/com/masai/modelRequestDto/CommentRequestDto.java
@@ -3,6 +3,10 @@
  */
 package com.masai.modelRequestDto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +21,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentRequestDto {
 
+	@NotNull(message = "{Comment.field.invalid}")
+	@NotBlank(message = "{Comment.field.invalid}")
+	@NotEmpty(message = "{Comment.field.invalid}")
 	private String content;
 
 }
