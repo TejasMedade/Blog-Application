b6a68e6e23387870475e79717f2ba3652ccf1f86
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:26:55 +0530

    Update PostRequestDto.java

--- Blog_Application/src/main/java/com/masai/modelRequestDto/PostRequestDto.java
+++ Blog_Application/src/main/java/com/masai/modelRequestDto/PostRequestDto.java
@@ -4,6 +4,9 @@
 package com.masai.modelRequestDto;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,12 +21,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PostRequestDto {
 
+	@NotNull(message = "{Post.title.invalid}")
+	@NotBlank(message = "{Post.title.invalid}")
+	@NotEmpty(message = "{Post.title.invalid}")
 	private String title;
 
+	@NotNull(message = "{Post.content.invalid}")
+	@NotBlank(message = "{Post.content.invalid}")
+	@NotEmpty(message = "{Post.content.invalid}")
 	@Column(length = Integer.MAX_VALUE)
 	private String contentLine1;
 
-	@Column(length = Integer.MAX_VALUE)
+	@NotNull(message = "{Post.content.invalid}")
+	@NotBlank(message = "{Post.content.invalid}")
+	@NotEmpty(message = "{Post.content.invalid}")
+	@Column(length = Integer.MAX_VALUE) 
 	private String contentLine2;
 
 }
