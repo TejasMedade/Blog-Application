c444e1ed16556a362842e7e61c7880b0d6270a83
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:27:07 +0530

    Update UserRepo.java

--- Blog_Application/src/main/java/com/masai/repository/UserRepo.java
+++ Blog_Application/src/main/java/com/masai/repository/UserRepo.java
@@ -3,6 +3,7 @@
  */
 package com.masai.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,5 +21,7 @@ public interface UserRepo extends JpaRepository<User, Integer> {
 	Optional<User> findByEmail(String email);
 
 	boolean existsByEmail(String email);
+
+	List<User> findByNameContaining(String keyword);
 
 }
